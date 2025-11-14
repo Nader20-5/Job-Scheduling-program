21db6d11093c5da72c6a2bfadd9b2c2b58c3e8b3
youssef barhoma <Yusef_20230670@fci.helwan.edu.eg>
2025-11-14 21:13:48 +0200

    added the fitness_score function

--- cultural_algorithm.py
+++ cultural_algorithm.py
@@ -4,5 +4,9 @@ FINAL_POPULATION = 50
 FINAL_GENERATIONS = 100
 
 def solve_with_ca(jobs, machines):
-    population = [schedule.create_random_schedule(jobs , machines) for _ in range(FINAL_POPULATION)]
-    fitness = [schedule.calculate_fitness(s , jobs , machines) for s in population]
+    
+    #making 50 random schedules
+    population = [schedule.create_random_schedule(jobs , machines) for _ in range(FINAL_POPULATION)] 
+
+    #getting fitness score for each schedule
+    fitness_score = [schedule.calculate_fitness(s , jobs , machines) for s in population] 
